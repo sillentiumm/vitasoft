0c0cb0f13eb8726f0c724f1e832dacc5a8492438
sillentiumm <antony77022@gmail.com>
2025-07-20 10:56:47 +0300

    add dist

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
+  base: '/vitasoft/',
   plugins: [
     vue(),
     vueDevTools(),
